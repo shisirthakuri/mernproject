c345b0f614800cc95152131203e29c3316382f92
shisirthakuri <shahishisir087@gmail.com>
2025-03-10 21:45:01 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import Card from "../components/Card"
+import Navbar from "../components/Navbar"
+
+
+
+
+
+function Home(){
+    return(
+        <>
+        <Navbar/>
+
+        <div className='flex flex-wrap'>
+        <Card/>
+        <Card/>
+        <Card/>
+       
+        
+        
+        </div>
+    
+        
+        </>
+    )
+}
+export default Home 
